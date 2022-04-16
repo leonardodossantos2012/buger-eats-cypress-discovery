aaea26fc67af2b6e6dc4e3455c925e8ccd96f822
leonardodossantos2012 <leonardodossantos2012@hotmail.com>
2022-04-16 13:15:57 -0300

    subindo um cenário para trazer um bug

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -46,7 +46,7 @@ describe('Cadastro', () => {
         const messages = [
             { field: 'name', output: 'É necessário informar o nome' },
             { field: 'cpf', output: 'É necessário informar o CPF' },
-            { field: 'email', output: 'É necessário informar o email' },
+            { field: 'email', output: 'É necessário informar o e-mail' },
             { field: 'postalcode', output: 'É necessário informar o CEP' },
             { field: 'number', output: 'É necessário informar o número do endereço' },
             { field: 'delivery_method', output: 'Selecione o método de entrega' },
